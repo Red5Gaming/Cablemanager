ccaefcbf8c4454ed527e5752efccc134c43a9b9a
Red5Gaming <nicompf05@gmail.com>
2022-02-02 10:03:52 +0100

    Refresh button now logs Input Amount

--- cablemanager/src/app/header/header.component.ts
+++ cablemanager/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {components} from "../component-list/component-model/component-model.component";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {BehaviorSubject} from "rxjs";
@@ -17,6 +17,13 @@ export class HeaderComponent implements OnInit {
     description: new FormControl('')
   })
 
+  //Creates array with i length
+  arraylen(i: number) {
+   return new Array<number>(i).fill(1)
+  }
+
+inNumlen: number[]
+
 
   public inNum: number = this.fullForm.value.inNum //this.fullForm.value.inNum
 
@@ -30,17 +37,27 @@ export class HeaderComponent implements OnInit {
     return new Array(i)
   }
 
-  
+
 
 
   onClick() {
+
     //console.log("AAAAAAAAAAAAAAAAAA")
     console.log(this.fullForm.value.inNum)
     console.log(this.inNumArr)
     console.log(this.fullForm.value)
+
     this.componentService.addComponent(this.fullForm.value)
     this.fullForm.reset()
   }
+
+
+refreshInF() {
+    let inLenArr: number[] = this.arraylen(this.fullForm.value.inNum)
+  console.log(inLenArr)
+    return inLenArr
+
+}
 
 
 
@@ -51,9 +68,12 @@ export class HeaderComponent implements OnInit {
     this.fullForm = new FormGroup({
       name: new FormControl(null, Validators.required),
       desc: new FormControl(null, Validators.required),
+
+
       input: new FormControl(null, Validators.required),
+
       output: new FormControl(null, Validators.required),
-      inNum: new FormControl(1, [Validators.min(1), Validators.max(10), Validators.required])
+      inNum: new FormControl(null, [Validators.min(1), Validators.max(10), Validators.required])
     });
   }
 
